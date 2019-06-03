ee7a2f0558579049482e07e490aeef306afa7a83
angelo <iasevoli90@gmail.com>
2019-06-03 10:09:13 +0200

    chore: modify code to recreate my issue

--- script/consumer-create-pact.sh
+++ script/consumer-create-pact.sh
@@ -6,11 +6,21 @@ pact/bin/pact-mock-service start -p 1234 --pact-dir ./pacts --log ./log/foo-bar-
 # clear interactions (not necessary for this example, just showing how)
 curl -X DELETE -H "X-Pact-Mock-Service: true"  localhost:1234/interactions
 
-# set up interaction
+# set up interaction (application/json)
 curl -X POST -H "X-Pact-Mock-Service: true" -d@script/consumer-interaction.json localhost:1234/interactions
 
-# execute interaction
-curl localhost:1234/foo
+# set up interaction 2 (multipart)
+curl -X POST -H "X-Pact-Mock-Service: true" -d@script/consumer-interaction2.json localhost:1234/interactions
+
+# execute interaction (just the application/json one)
+curl -X POST \
+  http://localhost:1234/foo \
+  -H 'Content-Type: application/json' \
+  -H 'Host: localhost:1234' \
+  -H 'content-length: 19' \
+  -d '{
+	"query":"TEST"
+}'
 
 # verify interaction took place
 curl -H "X-Pact-Mock-Service: true" localhost:1234/interactions/verification
